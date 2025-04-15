f31f386695d988d35d1f18af99e29eb28a84f16b
rachidcooder <cooderherou@gmail.com>
2025-04-15 17:52:19 +0100

    export dara

--- src/controllers/generalCntroller.js
+++ src/controllers/generalCntroller.js
@@ -1,7 +1,8 @@
 import Product from "../models/Product.js";
 import Order from "../models/Order.js";
-import User from "../models/User.js";
+//import User from "../models/User.js";
 import Section from "../models/Section.js";
+import excelJS from 'exceljs'
 
 export const getAnalytics = async (req, res) => {
   try {
@@ -22,7 +23,7 @@ export const getAnalytics = async (req, res) => {
   }
 };
 
-export const getMonthlyStatistic = async(res,req)=>{
+export const getMonthlyStatistic = async(req,res)=>{
 
   try{
     const monthlyOrders= await Order.find()
@@ -32,3 +33,60 @@ export const getMonthlyStatistic = async(res,req)=>{
     res.status(500).json({ message: "Server error" });
   }
 }
+
+
+const Users = [
+  { firstname: "Amir", lastname: "Mustafa", email: "amir@gmail.com" },
+  { firstname: "Ashwani", lastname: "Jumar", email: "ashwani@gmail.com" },
+  { firstname: "Nupur", lastname: "Shah", email: "nupur@gmail.com" },
+  { firstname: "Himanshu", lastname: "Mewari", email: "himanshu@gmail.com" },
+  { firstname: "Vankayala", lastname: "Sirisha", email: "sirisha@gmail.com" },
+];
+
+export const ExportData = async (req, res) => {
+  console.log("Generating Excel file...");
+
+  const workbook = new excelJS.Workbook();
+  const worksheet = workbook.addWorksheet("My Users");
+
+  worksheet.columns = [
+    { header: "S No.", key: "s_no", width: 10 },
+    { header: "First Name", key: "firstname", width: 20 },
+    { header: "Last Name", key: "lastname", width: 20 },
+    { header: "Email", key: "email", width: 30 },
+  ];
+
+  let counter = 1;
+  Users.forEach((user) => {
+    user.s_no = counter++;
+    worksheet.addRow(user);
+  });
+
+  worksheet.getRow(1).eachCell((cell) => {
+    cell.font = { bold: true };
+  });
+
+  try {
+    // Set headers to trigger download in browser
+    res.setHeader(
+      "Content-Type",
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    );
+    res.setHeader(
+      "Content-Disposition",
+      "attachment; filename=users.xlsx"
+    );
+
+    // Write workbook to response as stream
+    await workbook.xlsx.write(res);
+
+    // End the response
+    res.end();
+  } catch (err) {
+    console.error("ExportData error:", err);
+    res.status(500).send({
+      status: "error",
+      message: "Failed to export data",
+    });
+  }
+};
--- src/routes/generaRoute.js
+++ src/routes/generaRoute.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import {getAnalytics} from '../controllers/generalCntroller.js'
+import {getAnalytics ,ExportData} from '../controllers/generalCntroller.js'
 import { protect ,protectAdmin}from '../middlwares/authMidlware.js'
 const route = express.Router()
 
 route.get('/analytics',protect,protectAdmin,getAnalytics);
+
+route.get('/exportdata',ExportData);
 
 
 export default route
